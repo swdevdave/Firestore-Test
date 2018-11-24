30df6c13785e26fde0d80eb4bb7791af948c10d3
swdevdave <swdev.dave@gmail.com>
2018-11-23 21:54:19 -0800

    Added update only

--- app/src/main/java/com/swdave/firestoretest/MainActivity.java
+++ app/src/main/java/com/swdave/firestoretest/MainActivity.java
@@ -17,7 +17,6 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -100,11 +99,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateDescription(View view) {
         String description = editTextDescription.getText().toString();
-        
-        Map<String, Object> note = new HashMap<>();
-        note.put(KEY_DESCRIPTION, description);
 
-        noteRef.set(note, SetOptions.merge());
+        /**  Below creates new document if it does not exist **/
+
+        //Map<String, Object> note = new HashMap<>();
+        //note.put(KEY_DESCRIPTION, description);
+        //noteRef.set(note, SetOptions.merge());
+
+        // Below only updates - if no document exists wont create.
+        noteRef.update(KEY_DESCRIPTION, description);
     }
 
 
