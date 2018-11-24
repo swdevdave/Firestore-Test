6f269c9ed569d1f30f718d0e63f85711ff350abf
swdevdave <swdev.dave@gmail.com>
2018-11-23 23:02:40 -0800

    Added Delete Description, Delete Document.

--- app/src/main/java/com/swdave/firestoretest/MainActivity.java
+++ app/src/main/java/com/swdave/firestoretest/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -18,9 +19,8 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import javax.annotation.Nullable;
 
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewData;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Notebook");
     private DocumentReference noteRef = db.document("Notebook/My First Note");
     private ListenerRegistration noteLisener;
 
@@ -54,25 +55,88 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+        notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
-                    Toast.makeText(MainActivity.this, "Error While Loading", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, e.toString());
                     return;
                 }
-                if (documentSnapshot.exists()) {
-                    Note note = documentSnapshot.toObject(Note.class);
 
+                String data = "";
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    Note note = documentSnapshot.toObject(Note.class);
+                    note.setDocumentId(documentSnapshot.getId());
+
+
+                    String documentId = note.getDocumentId();
                     String title = note.getTitle();
                     String description = note.getDescription();
-                    textViewData.setText("Title: " + title + "\n" + "Description: " + description);
-                } else {
-                    textViewData.setText("");
+
+                    data += "ID: " + documentId +
+                            "\nTitle: " + title +
+                            "\nDescription: " + description +
+                            "\n\n";
                 }
+                textViewData.setText(data);
             }
         });
+        // below is code for testing only
+//        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    Toast.makeText(MainActivity.this, "Error While Loading", Toast.LENGTH_SHORT).show();
+//                    Log.d(TAG, e.toString());
+//                    return;
+//                }
+//                if (documentSnapshot.exists()) {
+//                    Note note = documentSnapshot.toObject(Note.class);
+//
+//                    String title = note.getTitle();
+//                    String description = note.getDescription();
+//                    textViewData.setText("Title: " + title + "\n" + "Description: " + description);
+//                } else {
+//                    textViewData.setText("");
+//                }
+//            }
+//        });
+    }
+
+    public void addNote(View view) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+
+        Note note = new Note(title, description);
+
+        notebookRef.add(note); // can add onSuccess and onFailure listeners.
+    }
+
+    public void loadNotes(View view) {
+        notebookRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Note note = documentSnapshot.toObject(Note.class);
+                            note.setDocumentId(documentSnapshot.getId());
+
+                            String documentId = note.getDocumentId();
+                            String title = note.getTitle();
+                            String description = note.getDescription();
+
+                            data += "ID: " + documentId +
+                                    "\nTitle: " + title +
+                                    "\nDescription: " + description +
+                                    "\n\n";
+
+                        }
+
+                        textViewData.setText(data);
+                    }
+                });
     }
 
     public void saveNote(View view) {
@@ -110,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
         noteRef.update(KEY_DESCRIPTION, description);
     }
 
-    public void deleteDescription(View view){
+    public void deleteDescription(View view) {
 
         //Map<String, Object> note = new HashMap<>();
         //note.put(KEY_DESCRIPTION,FieldValue.delete());
@@ -119,11 +183,10 @@ public class MainActivity extends AppCompatActivity {
         noteRef.update(KEY_DESCRIPTION, FieldValue.delete());
     }
 
-    public void deleteNote(View view){
+    public void deleteNote(View view) {
 
         noteRef.delete();
     }
-
 
     public void loadNote(View view) {
         noteRef.get()
@@ -148,8 +211,5 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, e.toString());
                     }
                 });
-
     }
-
-
 }
--- app/src/main/java/com/swdave/firestoretest/Note.java
+++ app/src/main/java/com/swdave/firestoretest/Note.java
@@ -1,7 +1,11 @@
 package com.swdave.firestoretest;
 
+import com.google.firebase.firestore.Exclude;
+
 public class Note {
 
+
+    private String documentId;
     private String title;
     private String description;
 
@@ -23,4 +27,14 @@ public class Note {
         return description;
     }
 
+
+    // @Exclude hides this from Firebase so it doesnt add it to the DB
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
 }
